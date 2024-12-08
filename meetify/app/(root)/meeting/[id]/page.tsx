ae687f02fca16c7c912a0d201560cfee07d9e1e9
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2024-12-09 01:09:04 +0600

    Meeting Setup Page with Camera and Mic Permissions Added

--- meetify/app/(root)/meeting/[id]/page.tsx
+++ meetify/app/(root)/meeting/[id]/page.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+'use client';
 
-const Meeting = ({ params }: { params: { id: String } }) => {
+import { useState } from 'react';
+import { useUser } from '@clerk/nextjs';
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import { useParams } from 'next/navigation';
+import { Loader } from 'lucide-react';
+
+import { useGetCallById } from '@/hooks/useGetCallById';
+import MeetingRoom from '@/components/Meeting/MeetingRoom';
+import MeetingSetup from '@/components/Meeting/MeetingSetup';
+
+const MeetingPage = () => {
+    const { id } = useParams();
+    const { isLoaded, user } = useUser();
+    const { call, isCallLoading } = useGetCallById(id);
+    const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+    if (!isLoaded || isCallLoading) return <Loader />;
+
+    if (!call) return (
+        <p className="text-center text-3xl font-bold text-white">
+            Stream Video Client Call Not Found
+        </p>
+    );
+
     return (
-        <div>Meeting Room: #{params.id}</div>
-    )
-}
+        <main className="h-screen w-full">
+            <StreamCall call={call}>
+                <StreamTheme>
 
-export default Meeting
+                    {!isSetupComplete ? (
+                        <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+                    ) : (
+                        <MeetingRoom />
+                    )}
+                </StreamTheme>
+            </StreamCall>
+        </main>
+    );
+};
+
+export default MeetingPage;
