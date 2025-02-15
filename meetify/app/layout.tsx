1458a5adb45aac3257f4997ecf9c002848cee884
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2025-02-15 15:38:00 +0600

    Added Meeting Room Functionalities

--- meetify/app/layout.tsx
+++ meetify/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster";
+import "@stream-io/video-react-sdk/dist/css/styles.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
